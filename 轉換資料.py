f6250881d6b877739e95b5adec554392079c0a36
薛丞宏 <ihcaoe@gmail.com>
2016-02-05 17:49:40 +0800

    有的檔案會無去 `原始資料/HL/演講/林文貞/tbts-52-我所了解e5台灣.txt`

---
+++ 轉換資料.py
@@ -0,0 +1,83 @@
+from os import walk, makedirs
+from os.path import join, dirname
+from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
+import re
+
+舊資料夾 = '原始資料'
+新資料夾 = '轉換後資料'
+
+
+def _轉碼(檔名, 檔案原來編碼):
+    新檔名 = 檔名.replace(舊資料夾, 新資料夾)
+    makedirs(dirname(新檔名), exist_ok=True)
+    程式腳本._走指令([
+        'iconv', '-f', 檔案原來編碼,
+        '-t', 'utf8',
+        '-o', 新檔名,
+        檔名
+    ])
+    return 新檔名
+
+
+def _試轉碼(檔名):
+    try:
+        return _轉碼(檔名,  'utf8')
+    except:
+        pass
+    try:
+        return _轉碼(檔名,  'BIG5')
+    except:
+        pass
+    try:
+        return _轉碼(檔名,  'BIG5HKSCS')
+    except:
+        pass
+    try:
+        return _轉碼(檔名,  'GB2312')
+    except:
+        #         print(檔名)
+        #         程式腳本._走指令(['file',檔名],True)
+        #         程式腳本._走指令(['enca',檔名],True)
+        raise RuntimeError(檔名 + '無法度轉！！')
+
+
+def _換空白(檔名):
+    with open(檔名, 'rb') as 檔案:
+        原本資料 = 檔案.read()
+    換空白資料 = re.sub(b'\x83.', 原本資料, b' ')
+#     換空白資料 = (原本資料
+#              .replace(b'\x83x', b' ')
+#              .replace(b'\x83n', b' ')
+#              .replace(b'\x83i', b' ')
+#              )
+    with open(檔名, 'wb') as 檔案:
+        檔案.write(換空白資料)
+#     print(原本資料)
+
+
+def _提掉x02_b控制字元(檔名):
+    with open(檔名, 'rb') as 檔案:
+        原本資料 = 檔案.read()
+    新資料 = (原本資料
+           .replace(b'\x02', b'')
+           .replace(b'\x0d', b'')
+           )
+    with open(檔名, 'wb') as 檔案:
+        檔案.write(新資料)
+
+
+if __name__ == '__main__':
+    for 所在, 資料夾, 檔案檔名 in walk(舊資料夾):
+        for 檔案 in 檔案檔名:
+            if 檔案.endswith('txt'):
+                檔名 = join(所在, 檔案)
+                try:
+                    新檔名 = _試轉碼(檔名)
+                except:
+                    try:
+                        _換空白(檔名)
+                        新檔名 = _試轉碼(檔名)
+                    except:
+                        print(檔名)
+                        raise
+                _提掉x02_b控制字元(新檔名)
