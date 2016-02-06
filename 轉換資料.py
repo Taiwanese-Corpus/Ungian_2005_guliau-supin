49933f4b03c15f47571b33ecb3eb2e4f8ce1ad44
薛丞宏 <ihcaoe@gmail.com>
2016-02-06 13:13:08 +0800

    會當轉就用iconv轉，袂使的檔案頭前就加`333pigu-`，用notepad轉 #1

--- 轉換資料.py
+++ 轉換資料.py
@@ -1,7 +1,7 @@
 from os import walk, makedirs
-from os.path import join, dirname
+from os.path import join, dirname, basename
 from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
-import re
+from shutil import copy2
 
 舊資料夾 = '原始資料'
 新資料夾 = '轉換後資料'
@@ -39,24 +39,12 @@ def _試轉碼(檔名):
         #         程式腳本._走指令(['file',檔名],True)
         #         程式腳本._走指令(['enca',檔名],True)
         #         raise RuntimeError(檔名 + '無法度轉！！')
-        程式腳本._走指令([
-            'iconv', '-f', 'big5',
-            '-t', 'utf8', '-c',  # 忽略有問題的字元
-            '-o', 新檔名,
-            檔名
-        ])
+        新檔名 = join(
+            dirname(檔名).replace(舊資料夾, 新資料夾),
+            '333pigu-' + basename(檔名)
+        )
+        copy2(檔名, 新檔名)
         return 新檔名
-
-
-def _提掉x02_b控制字元(檔名):
-    with open(檔名, 'rb') as 檔案:
-        原本資料 = 檔案.read()
-    新資料 = (原本資料
-           .replace(b'\x02', b'')
-           .replace(b'\x0d', b'')
-           )
-    with open(檔名, 'wb') as 檔案:
-        檔案.write(新資料)
 
 
 if __name__ == '__main__':
@@ -64,5 +52,4 @@ if __name__ == '__main__':
         for 檔案 in 檔案檔名:
             if 檔案.endswith('txt'):
                 檔名 = join(所在, 檔案)
-                新檔名 = _試轉碼(檔名)
-#                 _提掉x02_b控制字元(新檔名)
+                _試轉碼(檔名)
